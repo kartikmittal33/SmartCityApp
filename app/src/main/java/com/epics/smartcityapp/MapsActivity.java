c39310e8adb372d08d272ce5d9f1b1cab7e48bbe
kartikmittal33 <31484338+kartikmittal33@users.noreply.github.com>
2018-01-27 17:32:55 -0500

    Working but not functional app

--- app/src/main/java/com/epics/smartcityapp/MapsActivity.java
+++ app/src/main/java/com/epics/smartcityapp/MapsActivity.java
@@ -1,6 +1,9 @@
 package com.epics.smartcityapp;
 
+
+import android.app.AlertDialog;
 import android.app.ProgressDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.graphics.Bitmap;
 import android.os.AsyncTask;
@@ -9,8 +12,8 @@ import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
-import android.widget.EditText;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.google.android.gms.common.api.Status;
 import com.google.android.gms.location.places.Place;
@@ -27,154 +30,178 @@ import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
-import static com.epics.smartcityapp.R.id.map;
-
-//main activity to implement maps and camera functionality 
-
+//main activity to implement maps and camera functionality
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
     private static final int CAMERA_REQUEST = 1888;
-    Spinner spinner; //severity chooser object 
-    EditText edtAddress; //input address bar 
-    Button search; //search address bar   
-    private GoogleMap mMap; //map object 
+    int PLACE_AUTOCOMPLETE_REQUEST_CODE = 1;
+
+
+    private GoogleMap mMap;  //map object
+    Spinner spinner;  //severity chooser object
+    // EditText edtAddress;  //input address bar
+    String autoSelect;
+    Button search;  //search address bar
+    Button send;
+    private TextView popup;
+
 
     @Override
-
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);  // Obtain the SupportMapFragment and get notified when the map is ready to be used. 
-        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager().findFragmentById(map);
-        mapFragment.getMapAsync(this); //implement maps fragment   
-        spinner = (Spinner) findViewById(R.id.simplespinner); //initialize spinner 
-        edtAddress = (EditText) findViewById(R.id.btn_place); //initialize address bar
-        search = (Button) findViewById(R.id.search); //initialize search bar   
-        // Create an ArrayAdapter using the string array and a default spinner layout 
-        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this, R.array.severity, android.R.layout.simple_spinner_item);
-        // Specify the layout to use when the list of choices appears 
+        setContentView(R.layout.activity_maps);
+        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        mapFragment.getMapAsync(this);  //implement maps fragment
+        autoSelect = "Purdue Memorial Union, Grant Street, West Lafayette, IN";
+        spinner = (Spinner) findViewById(R.id.simplespinner);  //initialize spinner
+        // edtAddress = (EditText) findViewById(R.id.btn_place);       //initialize address bar
+        search = (Button) findViewById(R.id.search);        //initialize search bar
+        send = (Button) findViewById(R.id.send);
+
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.severity, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        // Apply the adapter to the spinner 
+
+        // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
+        spinner.setPrompt("Select your favorite Planet!");
+
         PlaceAutocompleteFragment autocompleteFragment = (PlaceAutocompleteFragment)
                 getFragmentManager().findFragmentById(R.id.place_autocomplete_fragment);
+
         autocompleteFragment.setOnPlaceSelectedListener(new PlaceSelectionListener() {
-//            @Override
-//
-//            public void onPlaceSelected(Place place) {
-//                autoSelect = place.getAddress().toString();
-//            }
-
-
             @Override
             public void onPlaceSelected(Place place) {
+                autoSelect=place.getAddress().toString();
+
             }
 
             @Override
+            public void onError(Status status) {
+                // TODO: Handle the error.
 
-            public void onError(Status status) { // TODO: Handle the error. }});
             }
-
-            //create new intent to take a picture 
-            public void takePic(View view) {
-                Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(cameraIntent, CAMERA_REQUEST);
-            }
-
-            public void getCords(View view) {
-                new GetCoordinates().execute(edtAddress.getText().toString().replace(" ", "+"));
-            }    //when camera is activated, take picture and return the photo as a bitmap
-
-            protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-                if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
-                    Bitmap photo = (Bitmap) data.getExtras().get("data");
-                }
-                if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE)
-
-                {
-                    if (resultCode == RESULT_OK) {
-                        Place place = PlaceAutocomplete.getPlace(this, data);
-                        autoSelect = place.getName().toString();
-                    } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
-                        Status status = PlaceAutocomplete.getStatus(this, data);
-                    } else if (resultCode == RESULT_CANCELED) { // The user canceled the operation.
-                    }
-                }
-            }
-
-            //create map and center it around west lala
-            @Override
-
-            public void onMapReady(GoogleMap googleMap) {
-                mMap = googleMap;
-                LatLng westL = new LatLng(40.424544, -86.918871); //make a LatLng object for newLatLng method parameter with west lala cords 
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(westL)); //move map around west lala 
-                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(westL, 15)); //zoom with 15, shows streets but not too zoomed   
-            }    //when address is searched, the get cordinates class returns cords, have to center map around cords
-
-            public void moveCenter(String lat, String lng) {
-                int ilat = Integer.parseInt(lat); //passed in string, need int for LatLng object 
-                int ilng = Integer.parseInt(lng); //same thing 
-                LatLng center = new LatLng(ilat, ilng); //create center LatLng object with cords 
-                mMap.moveCamera(CameraUpdateFactory.newLatLng(center)); //center the map around this spot
-            }
-            //works with HttpDataHandler (Geocoder) class to return cords from address
-
-            private class GetCoordinates extends AsyncTask<String, Void, String> {
-                ProgressDialog dialog = new ProgressDialog(MapsActivity.this);
-
-                //while the geocoder is executing, show a please wait message to not overload requests
-                @Override
-
-                protected void onPreExecute() {
-                    super.onPreExecute();
-                    dialog.setMessage("Please wait...");
-                    dialog.setCanceledOnTouchOutside(false);
-                    dialog.show();
-                }
-
-                @Override
-
-                protected String doInBackground(String... strings) {
-                    String response;
-                    try {
-                        String address = strings[0];
-                        HttpDataHandler http = new HttpDataHandler();
-                        String url = String.format("https://maps.googleapis.com/maps/api/geocode/json?address=%s", address);
-                        response = http.getHTTPData(url);
-                        return response;
-                    } catch (Exception ex) {
-                    }
-                    return null;
-                }
-
-                @Override
-
-                protected void onPostExecute(String s) {
-                    try {
-                        JSONObject jsonObject = new JSONObject(s);
-                        String lat = ((JSONArray) jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry").
-                                getJSONObject("location").get("lat").toString();
-                        String lng = ((JSONArray) jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry").
-                                getJSONObject("location").get("lng").toString();
-                        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(Double.parseDouble(lat), Double.parseDouble(lng)), 18));
-                        if (dialog.isShowing())
-
-                        {
-                            dialog.dismiss();
-                        }
-                    } catch (
-                            JSONException e)
-
-                    {
-                        e.printStackTrace();
-                    }
-                }
-            }
-
         });
+
     }
 
+    public void onDataSent(View v){
+        AlertDialog.Builder builder = new AlertDialog.Builder(MapsActivity.this);
+        builder.setCancelable(true);
+        builder.setTitle(null);
+        builder.setMessage("The data has been sent");
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.cancel();
+            }
+        });
+        builder.show();
+    }
+
+
+    //create new intent to take a picture
+    public void takePic(View view) {
+        Intent cameraIntent = new Intent(android.provider.MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivityForResult(cameraIntent, CAMERA_REQUEST);
+    }
+
+
+    public void getCords(View view) {
+
+        new GetCoordinates().execute(autoSelect.replace(" ", "+"));
+    }
+
+    //when camera is activated, take picture and return the photo as a bitmap
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == CAMERA_REQUEST && resultCode == RESULT_OK) {
+            Bitmap photo = (Bitmap) data.getExtras().get("data");
+            if (requestCode == PLACE_AUTOCOMPLETE_REQUEST_CODE) {
+                if (resultCode == RESULT_OK) {
+                    Place place = PlaceAutocomplete.getPlace(this, data);
+                    autoSelect=place.getName().toString();
+                } else if (resultCode == PlaceAutocomplete.RESULT_ERROR) {
+                    Status status = PlaceAutocomplete.getStatus(this, data);
+
+                } else if (resultCode == RESULT_CANCELED) {
+                    // The user canceled the operation.
+                }
+            }
+
+        }
+    }
+
+    //create map and center it around west lala
     @Override
     public void onMapReady(GoogleMap googleMap) {
+
+        mMap = googleMap;
+        LatLng westL = new LatLng(40.424544, -86.918871); //make a LatLng object for newLatLng method parameter with west lala cords
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(westL)); //move map around west lala
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(westL, 15)); //zoom with 15, shows streets but not too zoomed
+
+
+    }
+
+
+
+
+    //works with HttpDataHandler (Geocoder) class to return cords from address
+    private class GetCoordinates extends AsyncTask<String, Void, String> {
+        ProgressDialog dialog = new ProgressDialog(MapsActivity.this);
+
+        //while the geocoder is executing, show a please wait message to not overload requests
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+            dialog.setMessage("Please wait...");
+            dialog.setCanceledOnTouchOutside(false);
+            dialog.show();
+        }
+
+
+        @Override
+        protected String doInBackground(String... strings) {
+            String response;
+            try {
+                String address = strings[0];
+                HttpDataHandler http = new HttpDataHandler();
+                String url = String.format("https://maps.googleapis.com/maps/api/geocode/json?address=%s", address);
+                response = http.getHTTPData(url);
+                return response;
+            } catch (Exception ex) {
+
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            try {
+                JSONObject jsonObject = new JSONObject(s);
+
+                String lat = ((JSONArray) jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry")
+                        .getJSONObject("location").get("lat").toString();
+
+                String lng = ((JSONArray) jsonObject.get("results")).getJSONObject(0).getJSONObject("geometry")
+                        .getJSONObject("location").get("lng").toString();
+
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(Double.parseDouble(lat),Double.parseDouble(lng)),18));
+
+                if (dialog.isShowing()) {
+                    dialog.dismiss();
+                }
+
+
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+        }
+
+
 
     }
 }
